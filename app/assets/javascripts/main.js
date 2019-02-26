4ff90b6eabf0bfe9efc653439e287936977d88d7
vdepetigny <vdepetigny@yahoo.fr>
2019-02-26 18:17:49 +0100

    1ere fonctionnalité

---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,10 @@
+// Animer les listes
+$(function() {
+  $('.card').hide();
+  $('.tabs').each(function(){
+    $(this).css('cursor','pointer').click(function(){
+      $(this).next().toggle();
+    })
+  });
+});
+
